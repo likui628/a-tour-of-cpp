93b836eefd699512a266f3036b261cc96ad1a662
likui628 <90845831+likui628@users.noreply.github.com>
2024-04-21 20:18:23 +0800

    struct

--- main.cpp
+++ main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
 
+using namespace std;
+
+struct Vector {
+	double* elm;
+	int sz;
+};
+
+void vector_init(Vector& v,int s) {
+	v.elm = new double[s];
+	v.sz = s;
+}
+
+double read_and_sum(int s) {
+	Vector v;
+	vector_init(v, s);
+	for (int i = 0; i < s; ++i) {
+		cin >> v.elm[i];
+	}
+
+	double sum = 0;
+	for (int i = 0; i < s; ++i) {
+		sum += v.elm[i];
+	}
+	return sum;
+}
+
+
 int main() {
-	std::cout << "Hello World" << std::endl;
+	double sum=read_and_sum(10);
+	cout << sum;
 }
