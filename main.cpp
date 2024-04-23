e60dadcd1d88c8b4a8cf111ea501eb06ebbd5d85
likui628 <90845831+likui628@users.noreply.github.com>
2024-04-24 07:51:03 +0800

    classes

--- main.cpp
+++ main.cpp
@@ -1,27 +1,17 @@
 #include <iostream>
+#include "Vector.h"
 
 using namespace std;
 
-struct Vector {
-	double* elm;
-	int sz;
-};
-
-void vector_init(Vector& v,int s) {
-	v.elm = new double[s];
-	v.sz = s;
-}
-
 double read_and_sum(int s) {
-	Vector v;
-	vector_init(v, s);
+	Vector v(s);
 	for (int i = 0; i < s; ++i) {
-		cin >> v.elm[i];
+		cin >> v[i];
 	}
 
 	double sum = 0;
 	for (int i = 0; i < s; ++i) {
-		sum += v.elm[i];
+		sum += v[i];
 	}
 	return sum;
 }
