f6f8e2765fa3cf426dbca180a3ce6010a67acc2d
likui628 <90845831+likui628@users.noreply.github.com>
2024-04-25 07:31:08 +0800

    structured binding

--- main.cpp
+++ main.cpp
@@ -3,41 +3,20 @@
 
 using namespace std;
 
-struct Node
-{
-	int data;
-	Node* next;
-};
-
 struct Entity {
 	string name;
-	variant<int, Node*> v;
+	int value;
 };
 
-void PrintEntity(const Entity& e) {
-	if (holds_alternative<int>(e.v)) {
-		cout << get<int>(e.v) << std::endl;
-	}
-	else {
-		std::cout << get<Node*>(e.v)->data << std::endl;
-	}
+Entity read_entity(istream& is) {
+	string name;
+	int value;
+	is >> name >> value;
+	return { name,value };
 }
 
-// the return type is "double"
-auto mul(int i, double d) -> double {
-	return i * d;
-}
 
 int main() {
-	Entity e_num{ "Sample Entity",32 };
-	PrintEntity(e_num);
-
-	Node* node = new Node{ 123, nullptr };
-	Entity e_ptr{ "Node Entity", node };
-	PrintEntity(e_ptr);
-
-	delete node;
-
-	auto mulRes=mul(3, 7);
-	std::cout << mulRes << std::endl;
+	auto [name, value] = read_entity(std::cin);
+	std::cout << name << " " << value << std::endl;
 }
