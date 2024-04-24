0687e244f2e850bfeed532b42702d3992b61c40c
likui628 <90845831+likui628@users.noreply.github.com>
2024-04-24 12:49:50 +0800

    enumerations

--- main.cpp
+++ main.cpp
@@ -16,8 +16,36 @@ double read_and_sum(int s) {
 	return sum;
 }
 
+enum TrafficLight {
+	green, yellow, red
+};
+
+TrafficLight& operator++(TrafficLight& t) {
+	switch (t)
+	{
+	case red:
+		t = green;
+		break;
+	case green:
+		t = yellow;
+		break;
+	case yellow:
+		t = red;
+		break;
+	}
+	return t;
+}
 
 int main() {
-	double sum=read_and_sum(10);
-	cout << sum;
+	//double sum = read_and_sum(10);
+	//cout << sum;
+
+	TrafficLight light = TrafficLight::red;
+	std::cout << light << std::endl;
+	++light;
+	std::cout << light << std::endl;
+	++light;
+	std::cout << light << std::endl;
+	++light;
+	std::cout << light << std::endl;
 }
