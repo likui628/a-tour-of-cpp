c90a9c1a567a06ad78251b33fc7d418219ec6cb5
likui628 <90845831+likui628@users.noreply.github.com>
2024-05-04 14:29:07 +0800

    I/O state

--- main.cpp
+++ main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <regex>
+#include <vector>
 
 using namespace std;
 
-int main() {
-	std::string text = "Roses are #ff0000, violets are #0000ff all of my base are belong to you";
+vector<int> read_ints(istream& is, const string& terminator)
+{
+	vector<int> res;
+	for (int i; is >> i; )
+		res.push_back(i);
 
-	std::string t = text;
-	std::regex r{ R"(#([a-fA-F0-9]{6}|[a-fA-F0-9]{3}))" };
-
-	for (std::smatch sm; std::regex_search(t, sm, r);) {
-		std::cout << sm.str() << std::endl;	
-		t = sm.suffix();
+	if (is.eof())                            // fine: end of file
+		return res;
+	if (is.fail()) {                         // we failed to read an int; was it the terminator?
+		is.clear();                          // reset the state to good()
+		string s;
+		if (is >> s && s == terminator)
+			return res;
+		is.setstate(ios_base::failbit);     // add fail() to is's state
 	}
+	return res;
+}
 
-	std::cout << text << std::endl;
+
+
+int main() {
+	auto v = read_ints(cin, "stop");
 }
