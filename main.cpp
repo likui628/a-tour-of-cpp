5e0f438de30aa04cdad10213de6c4f16413d8d5f
likui628 <90845831+likui628@users.noreply.github.com>
2024-05-04 09:05:19 +0800

    string view

--- main.cpp
+++ main.cpp
@@ -2,19 +2,40 @@
 #include "Vector.h"
 
 using namespace std;
-using namespace std::literals::string_literals;
+using namespace std::literals::string_view_literals;
+
+string cat(string_view sv1, string_view sv2)
+{
+	string res{ sv1 };           // initialize from sv1
+	return res += sv2;           // append from sv2 and return
+
+}
+
+void print_lower(string_view sv) {
+	for (char c : sv) {
+		std::cout << (char)tolower(c) << std::endl;
+	}
+}
+
+string_view bad()
+{
+	string s = "Once upon a time";
+	return { &s[5],4 };                // bad: returning a pointer to a local
+}
 
 int main() {
-	string name = "Niels Stroustrup";
+	string king = "Harold";
+	auto s1 = cat(king, "William");                              // HaroldWilliam: string and const char*
+	auto s2 = cat(king, king);                                   // HaroldHarold: string and string
+	auto s3 = cat("Edward", "Stephen"sv);                        // EdwardStephen: const char * and string_view
+	auto s4 = cat("Canute"sv, king);                             // CanuteHarold
+	auto s5 = cat({ &king[0],2 }, "Henry"sv);                    // HaHenry
+	auto s6 = cat({ &king[0],2 }, { &king[2],4 });               // Harold
+	std::cout << s1 << "\n" << s2 << "\n" << s3 << "\n" << s4 << "\n" << s5 << "\n" << s6 << std::endl;
 
+	print_lower(king);
 
-	string s = name.substr(6, 10);            // s = "Stroustrup"
-	name.replace(0, 5, "nicholas1sss");          // name becomes "nicholas Stroustrup"
-	name[0] = toupper(name[0]);            // name becomes "Nicholas Stroustrup"
-	std::cout << name << std::endl;
-
-	auto cat = "Cat"s;
-
-	std::u8string utf = u8"ÄãºÃÖÐÎÄ£¡";
-	std::cout << reinterpret_cast<const char*>(utf.c_str()) << std::endl;
+	auto b = bad();
+	// The behavior of out-of-range access to a string_view is undefined. 
+	std::cout << b << std::endl;
 }
