83362457804a1eea1835bf9db44442bf635f3de9
likui628 <90845831+likui628@users.noreply.github.com>
2024-05-04 15:44:08 +0800

    filesystem

--- main.cpp
+++ main.cpp
@@ -1,48 +1,49 @@
 #include <iostream>
-#include <regex>
-#include <vector>
+#include <fstream>
+#include <filesystem>
 
 using namespace std;
-
-struct Entry {
-	string name;
-	int number;
-};
-
-ostream& operator<<(ostream& os, const Entry& e)
-{
-	return os << "{\"" << e.name << "\", " << e.number << "}";
-}
-
-istream& operator>>(istream& is, Entry& e) {
-	return is >> e.name >> e.number;
-}
-
-vector<int> read_ints(istream& is, const string& terminator)
-{
-	vector<int> res;
-	for (int i; is >> i; )
-		res.push_back(i);
-
-	if (is.eof())                            // fine: end of file
-		return res;
-	if (is.fail()) {                         // we failed to read an int; was it the terminator?
-		is.clear();                          // reset the state to good()
-		string s;
-		if (is >> s && s == terminator)
-			return res;
-		is.setstate(ios_base::failbit);     // add fail() to is's state
-	}
-	return res;
-}
-
-
+namespace fs = std::filesystem;
 
 int main() {
-	Entry e1{ "entry",123 };
-	std::cout << e1 << std::endl;
+	fs::path dir = fs::current_path() / "example_dir";
 
-	Entry e2;
-	std::cin >> e2;
-	std::cout << e2 << std::endl;
+	// Create a directory
+	if (!fs::exists(dir)) {
+		fs::create_directory(dir);
+		std::cout << "Directory created: " << dir << std::endl;
+	}
+
+	// Path to a file inside the directory
+	fs::path file_path = dir / "example_file.txt";
+
+	// Write to a file
+	std::ofstream file_write(file_path);
+	if (file_write.is_open()) {
+		file_write << "Hello, Filesystem!";
+		file_write.close();
+		std::cout << "File written: " << file_path << std::endl;
+	}
+	else {
+		std::cerr << "Unable to open file for writing: " << file_path << std::endl;
+		return 1;
+	}
+
+	std::ifstream file_read(file_path);
+	if (file_read.is_open()) {
+		std::string line;
+		while (std::getline(file_read, line)) {
+			std::cout << "File read: " << line << std::endl;
+		}
+		file_read.close();
+	}
+	else {
+		std::cerr << "Unable to open file for reading: " << file_path << std::endl;
+		return 1;
+	}
+
+	std::cout << "list directory entry: " << std::endl;
+	// directory_iterator can be iterated using a range-for loop
+	for (auto const& dir_entry : std::filesystem::directory_iterator{ dir })
+		std::cout << dir_entry.path() << '\n';
 }
