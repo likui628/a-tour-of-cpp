2735f9716e89b80023449ff0737885ca6a6f135f
likui628 <90845831+likui628@users.noreply.github.com>
2024-05-05 17:52:34 +0800

    unordered_set

--- main.cpp
+++ main.cpp
@@ -1,20 +1,61 @@
 #include <iostream>
-#include "Vector.h"
+#include <string>
+#include <unordered_set>
 
 using namespace std;
 
+struct Record {
+	string name;
+	int product_code;
+};
+
+struct RecordHash {
+	size_t operator()(const Record& r)const {
+		size_t h1 = hash<string>()(r.name);
+		size_t h2 = hash<int>()(r.product_code);
+		return h1 ^ (h2 << 1); // Shift h2 to avoid collision when h1 is similar
+	}
+};
+
+struct RecordEqual {
+	bool operator()(const Record& a, const Record& b) const {
+		return a.name == b.name && a.product_code == b.product_code;
+	}
+};
+
+ostream& operator<<(ostream& os, const Record& r) {
+	os << "{\n" << r.name << ",\n" << r.product_code << ",\n}" << std::endl;
+	return os;
+}
 
 int main() {
-	Vector<int> v;
-	v.push_back(1);
-	v.push_back(2);
-	v.push_back(3);
-	v.push_back(4);
-	v.push_back(5);
-	v.push_back(6);
+	unordered_set<Record, RecordHash, RecordEqual> my_set;
+	Record r1{ "record1",1 };
+	Record r2{ "record2",2 };
+	Record r3{ "record3",3 };
+	Record r4{ "record4",4 };
 
-	std::cout << v[1] << std::endl;
-	std::cout << v.size() << std::endl;
-	std::cout << v.capacity() << std::endl;
-	std::cout << v.at(3) << std::endl;
+	my_set.insert(r1);
+	my_set.insert(r2);
+	my_set.insert(r3);
+	my_set.insert(r4);
+
+	auto result_1 = my_set.find({ "record2",2 });
+	if (result_1 != my_set.end()) {
+		std::cout << *result_1 << std::endl;
+	}
+	else {
+		std::cout << "Record not found." << std::endl;
+	}
+
+
+	auto result_2 = my_set.find({ "record1",2 });
+	if (result_2 != my_set.end()) {
+		std::cout << *result_2 << std::endl;
+	}
+	else {
+		std::cout << "Record not found." << std::endl;
+	}
+
+
 }
